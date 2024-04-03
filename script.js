42068a6cac6e018ee2ff43afcfce8b7c1ba01eb0
mrnayaa <malam563@newschool.edu>
2024-04-03 19:06:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 
 
 $(document).ready(function() {
-    // Event listener for navigation links
+    
     $('.nav-item').click(function(event) {
-        event.preventDefault(); // Prevent default link behavior
-
-        // Get the data-gif attribute value
+        event.preventDefault(); 
+        
         var gifSrc = $(this).data('gif');
 
-        // Remove existing GIFs
+       
         $('.gif-item').remove();
 
-        // Create and append multiple copies of the GIF to the grid container
-        for (var i = 0; i < 60; i++) { // 50 GIFs for demonstration
+        
+        for (var i = 0; i < 60; i++) { 
             var $newGif = $('<img>', {
                 class: 'gif-item',
                 src: gifSrc,
